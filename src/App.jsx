151d50c3d17fda51a502025e71aacb1c1f785016
sanjanaramgarhia <sanjanaramgarhia@gmail.com>
2026-06-28 17:35:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import HelloWorld from './components/HelloWorld'
+import LearnReact from './components/LearnReact'
+import LearnJSX from './components/LearnJSX'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>App Component</h1>
+      {/* <HelloWorld />
+      <LearnReact /> */}
+      <LearnJSX />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LearnJSX.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LearnJSX() {
+  return (
+    <div>LearnJSX</div>
+  )
+}
+
+export default LearnJSX
