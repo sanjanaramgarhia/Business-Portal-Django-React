5bb603e2207343200a6a0e1b0919501ef4409eb0
sanjana_tech <sanjanaramgarhia@gmail.com>
2026-09-03 13:58:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, Request
 from src.utils.settings import settings
 from src.tasks.models import TaskModel
+from src.tasks.urls import task_routes
+from src.utils.db import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
-
 app = FastAPI()
+app.include_router(task_routes)
